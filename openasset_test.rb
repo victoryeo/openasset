b60795d9669621aa29bf00a50ee31f204759edb8
victor <victoryeo1974@gmail.com>
2018-01-18 02:16:50 -0800

    mine the block after issue_asset

--- openasset_test.rb
+++ openasset_test.rb
@@ -3,6 +3,8 @@ require 'openassets'
 require 'net/http'
 require 'json'
 
+include ::OpenAssets::Util
+
 Bitcoin.network = :regtest
 RPCUSER = "alice"
 RPCPASSWORD = "alice"
@@ -27,8 +29,8 @@ api = OpenAssets::Api.new({
        min_confirmation:            1,
        max_confirmation:      9999999,
        rpc: {
-         user:                  'alice',
-         password:              'alice',
+         user:                 'alice',
+         password:             'alice',
          schema:               'http',
          port:                  18443,
          host:            'localhost',
@@ -36,26 +38,30 @@ api = OpenAssets::Api.new({
          open_timeout:             60 }
    })
 
-api.provider.list_unspent
-api.get_balance
+#api.provider.list_unspent
+#api.get_balance
 
 alice_key = Bitcoin::Key.generate
 bob_key = Bitcoin::Key.generate
 alice_key_priv = alice_key.to_base58
+bob_key_priv = bob_key.to_base58
 print "alice key " , alice_key_priv, "\n"
 bitcoinRPC('importprivkey',[alice_key_priv])
-api.provider.import_privkey(alice_key_priv)
+bitcoinRPC('importprivkey',[bob_key_priv])
+#api.provider.importprivkey(alice_key_priv, "alice", false)
 
-aa = bitcoinRPC('generatetoaddress',[80, alice_key.addr] )
-bb = bitcoinRPC('generatetoaddress',[40, bob_key.addr] )
+aa = bitcoinRPC('generatetoaddress',[200, alice_key.addr] )
+bb = bitcoinRPC('generatetoaddress',[10, bob_key.addr] )
 cc = bitcoinRPC('sendtoaddress',[alice_key.addr, 80] )
 
-include ::OpenAssets::Util
-
+alice_addr = bitcoinRPC('getaddressesbyaccount',["alice"])
 alice_oa_addr = address_to_oa_address(alice_key.addr)
 bob_oa_addr = address_to_oa_address(bob_key.addr)
 
-aa = bitcoinRPC('generatetoaddress',[80, alice_oa_addr] )
+# aa = bitcoinRPC('generatetoaddress',[80, alice_oa_addr] )
+
+asset_id = generate_asset_id(alice_key.pub)
+print "Asset id  ", asset_id , "\n"
 
 # api.issue_asset(<issuer open asset address>,
 #                 <issuing asset quantity>,
@@ -65,10 +71,22 @@ aa = bitcoinRPC('generatetoaddress',[80, alice_oa_addr] )
 #                 <mode=('broadcast', 'signed', 'unsigned')>,
 #                 <output_qty default value is 1.>)
 
-tx = api.issue_asset(alice_oa_addr, 150, 'AGHhobo7pVQN5fZWqv3rhdc324ryT7qVTB', alice_oa_addr, nil, 'broadcast')
-print "Tx " , tx.to_payload.bth , "\n"
-dd = bitcoinRPC('decoderawtransaction', tx.to_payload.bth)
-print "Decode ", dd, "\n"
+address = 'akEJwzkzEFau4t2wjbXoMs7MwtZkB8xixmH'
+#tx = api.issue_asset(address, 125, 'u=https://goo.gl/bmVEuw', address, nil, 'broadcast', 1)
+
+tx = api.issue_asset(alice_oa_addr, 125, 'u=https://goo.gl/bmVEuw', alice_oa_addr, 5000, 'broadcast', 1)
+# print "Tx " , tx.to_payload.bth , "\n"
+dd = bitcoinRPC('decoderawtransaction', [tx.to_payload.bth])
+#print "Decode ", dd, "\n"
+# sig = Bitcoin::Builder::TxBuilder.get_script_sig(alice_key)
+redeem_script = 0
+#sig_hash = tx.signature_hash_for_input(0, redeem_script)
+#script_sig = Bitcoin::Script.to_p2sh_multisig_script_sig(redeem_script)
+#script_sig_a = Bitcoin::Script.add_sig_to_multisig_script_sig(alice_key.sign(sig_hash), script_sig)
+
+print "Unspent  " , api.list_unspent([alice_oa_addr]) , "\n"
+
+aa = bitcoinRPC('generatetoaddress',[200, alice_key.addr] )
 
 # api.send_asset(<from open asset address>,
 #                <asset ID>,
@@ -78,7 +96,10 @@ print "Decode ", dd, "\n"
 #                <mode=('broadcast', 'signed', 'unsigned')>,
 #                <output_qty default value is 1.>)
 
+print "Address " , alice_oa_addr, "  " , alice_key.addr, "\n"
+print "Addressss " , address_to_oa_address(alice_key.addr), "\n"
 print "Balance " , api.get_balance(alice_oa_addr), "\n"
+print "Balance " , api.provider.getbalance(alice_oa_addr), "\n"
 
 # example
-api.send_asset(alice_oa_addr, 'AWo3R89p5REmoSyMWB8AeUmud8456bRxZL', 10, bob_oa_addr, nil, 'broadcast')
+api.send_asset(alice_oa_addr, asset_id, 10, bob_oa_addr, nil, 'broadcast')
